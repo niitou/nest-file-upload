c51e3a72027f7f14695e50a9ff1e26f78e772d14
noel <noelnolife@gmail.com>
2025-01-06 12:49:23 +0700

    import serve static and multer modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      serveRoot: '/public/',
+    }),
+    MulterModule.register({
+      storage : diskStorage({
+        destination : './public',
+        filename : (req, file, cb) => {
+          const filename = `${Date.now()}-${file.originalname}`
+          cb(null, filename)
+        }
+      })
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
