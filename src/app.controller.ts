90f0317ba418f7aaaec262b3e17181d9d349b589
noel <noelnolife@gmail.com>
2025-01-06 12:49:05 +0700

    upload single file and multiple files

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,10 +3,11 @@ import {
   Get,
   Post,
   UploadedFile,
+  UploadedFiles,
   UseInterceptors,
 } from '@nestjs/common';
 import { AppService } from './app.service';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { AnyFilesInterceptor, FileInterceptor } from '@nestjs/platform-express';
 
 @Controller()
 export class AppController {
@@ -17,9 +18,19 @@ export class AppController {
     return this.appService.getHello();
   }
 
+  // Upload Single File
   @Post('upload')
-  @UseInterceptors(FileInterceptor('file', { dest: './uploads/' }))
-  uploadFile(@UploadedFile() file: Express.Multer.File) {
-    console.log(file);
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file : Express.Multer.File) {
+    console.log("Single File")
+    return file
   }
+
+  // Upload Multiple Files
+  @Post('uploads')
+  @UseInterceptors(AnyFilesInterceptor())
+  uploadFiles(@UploadedFiles() files: Array<Express.Multer.File>) {
+    console.log("Multiple Files")
+    return files
+  }  
 }
