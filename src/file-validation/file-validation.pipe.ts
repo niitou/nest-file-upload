de0d0277a88ce89a9398a0c4a8737e7adb9cc279
noel <noelnolife@gmail.com>
2025-01-08 10:03:24 +0700

    remove file if fail validation

--- src/file-validation/file-validation.pipe.ts
+++ src/file-validation/file-validation.pipe.ts
@@ -1,22 +1,37 @@
-import { ArgumentMetadata, BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
-import { extname } from 'path';
-
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+import { extname, join } from 'path';
+import * as fs from 'fs';
 @Injectable()
 export class FileValidationPipe implements PipeTransform {
   private readonly maxFileSize = 10 * 1000000; // 10 MB
   private readonly allowedExtensions = ['.png', '.jpg', '.mp4'];
 
+  // Delete file
+  removeFile(value: any, message: string) {
+    fs.unlink(join(__dirname + '/../../public/' + value.filename), (err) => {
+      if (err) throw err;
+      console.log(`${value.filename} is deleted`);
+    });
+    throw new BadRequestException(message);
+  }
+
   transform(value: any, metadata: ArgumentMetadata) {
+    console.log(value);
     const extension = extname(value.originalname);
     const fileSize = value.size;
     if (!this.allowedExtensions.includes(extension)) {
-      throw new BadRequestException(`File type ${extension} is not supported`)
+      this.removeFile(value, `File type ${extension} is not supported`);
     }
 
     if (fileSize > this.maxFileSize) {
-      throw new BadRequestException(`File size is too large`)
+      this.removeFile(value, `File size is too large`);
     }
 
-    return value
+    return value;
   }
 }
--- src/files-validation/files-validation.pipe.ts
+++ src/files-validation/files-validation.pipe.ts
@@ -1,27 +1,46 @@
-import { ArgumentMetadata, BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
-import { extname } from 'path';
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+import { extname, join } from 'path';
+import * as fs from 'fs';
 
 @Injectable()
 export class FilesValidationPipe implements PipeTransform {
   private readonly maxFileSize = 5 * 1000000; // 5 MB
   private readonly allowedExtensions = ['.png', '.jpg'];
 
+  // Delete file
+  removeFile(filename: any, message: string) {
+    fs.unlink(join(__dirname + '/../../public/' + filename), (err) => {
+      if (err) throw err;
+      console.log(`${filename} is deleted`);
+    });
+    throw new BadRequestException(message);
+  }
+
   transform(value: Array<any>, metadata: ArgumentMetadata) {
-    const extensions = value.map(val => extname(val.originalname))
-    const fileSizes = value.map(val => val.size)
+    const extensions = value.map((val) => extname(val.originalname));
+    const fileSizes = value.map((val) => val.size);
+    const fileNames = value.map((val) => val.filename);
 
     fileSizes.forEach((fileSize, index) => {
-      const extension = extensions[index]
+      const extension = extensions[index];
 
       if (!this.allowedExtensions.includes(extension)) {
-        throw new BadRequestException(`File type ${extension} is not supported`)
+        this.removeFile(
+          fileNames[index],
+          `File type ${extension} is not supported`,
+        );
       }
 
       if (fileSize > this.maxFileSize) {
-        throw new BadRequestException(`File size is too large`)
+        this.removeFile(fileNames[index], `File size is too large`);
       }
     });
 
-    return value
+    return value;
   }
 }
